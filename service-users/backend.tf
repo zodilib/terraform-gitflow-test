22959823ea246f0b2e7a1d971713e960587aa2fa
Abhishek Srivastava <abhishek.srivasta@hyphengroup.io>
2022-07-21 20:52:01 +0800

    Adding the git flow

--- service-users/backend.tf
+++ service-users/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "do-not-delete-data-terraform-state-backend"
+    bucket         = "xxx"
     key            = "service-users/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "data_terraform_state"
-    profile = "data-dev"
+    dynamodb_table = "xxx"
   }
 }
