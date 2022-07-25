0f6787546b1cb2d25c447e4a3c07bedaadfa79c5
Abhishek Srivastava <abhishek.srivasta@hyphengroup.io>
2022-07-25 17:02:10 +0800

    Adding service now module

--- service-users/backend.tf
+++ service-users/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "xxx"
+    bucket         = "do-not-delete-data-terraform-state-backend"
     key            = "service-users/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "xxx"
+    dynamodb_table = "data_terraform_state"
+    #profile = "data-dev"
   }
 }
